9332372ca4f624d1163b0cb09361bb7c28b68c4d
youssefkl12 <youssefkhaledmahmoud12@gmail.com>
2023-05-09 20:09:31 +0300

    webCrawler start

---
+++ SSE/src/main/java/dev/se/SSE/ReadSeed.java
@@ -0,0 +1,33 @@
+package dev.se.SSE;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class ReadSeed {
+    Queue<String> seedsQeue = new LinkedList<> ();
+    BufferedReader reader;
+    public  ReadSeed(){
+        try {
+            reader = new BufferedReader(new FileReader("seed.txt"));
+            String line = reader.readLine();
+            seedsQeue.add(line);
+
+            while (line != null) {
+                //System.out.println(line);
+                // read next line
+                line = reader.readLine();
+                seedsQeue.add(line);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+}
