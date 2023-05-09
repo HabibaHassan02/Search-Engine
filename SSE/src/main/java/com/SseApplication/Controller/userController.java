62d9256f52a655d927200846950a842612ce7d9b
Omar Elsendiony <82820327+Omar-Elsendiony@users.noreply.github.com>
2023-05-09 20:23:13 +0300

    added n-tier architecture needed for spring boot:

---
+++ SSE/src/main/java/com/SseApplication/Controller/userController.java
@@ -0,0 +1,38 @@
+package com.SseApplication.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SseApplication.Entity.User;
+import com.SseApplication.Repository.UserRepository;
+
+@RestController
+public class userController {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	// Save method is predefine method in Mongo Repository
+	// with this method we will save user in our database
+	@GetMapping("/")
+	public int addUser() {
+		return 1;
+	}
+	
+	@PostMapping("/addUser")
+	public User addUser(@RequestBody User user) {
+		return userRepo.save(user);
+	}
+	
+	// findAll method is predefine method in Mongo Repository
+	// with this method we will all user that is save in our database
+	@GetMapping("/getAllUser")
+	public List<User> getAllUser(){
+		return userRepo.findAll();
+	}
+}
--- SSE/src/main/java/com/SseApplication/SseApplication.java
+++ SSE/src/main/java/com/SseApplication/SseApplication.java
@@ -1,4 +1,4 @@
-package dev.se.SSE;
+package com.SseApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
