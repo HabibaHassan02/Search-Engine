49eac22d0c4156f0cfc3100688760d14d3fa4ab1
youssefkl12 <youssefkhaledmahmoud12@gmail.com>
2023-05-13 15:38:23 +0300

    crawler

---
+++ SSE/src/main/java/com/SseApplication/Counter.java
@@ -0,0 +1,17 @@
+package com.SseApplication;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+        count = 0;
+    }
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
