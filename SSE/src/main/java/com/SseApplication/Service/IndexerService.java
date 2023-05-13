00b6066e850aa61007e8cf03e7d854faccdbc7a9
Omar Elsendiony <82820327+Omar-Elsendiony@users.noreply.github.com>
2023-05-13 15:37:11 +0300

    adding without indexer

---
+++ SSE/src/main/java/com/SseApplication/Service/IndexerService.java
@@ -0,0 +1,18 @@
+//package com.SseApplication.Service;
+//
+//import java.util.HashMap;
+//
+//import org.springframework.data.mongodb.core.index.Index;
+//import org.springframework.stereotype.Service;
+//
+//import com.SseApplication.Entity.PageData;
+//import com.SseApplication.Repository.IndexerRepository;
+//
+//@Service
+//public class IndexerService {
+//	IndexerRepository indexerRepo;
+//	HashMap<String,PageData > hm = new HashMap<String,PageData>();
+//	public void addToIndexer() {
+//		indexerRepo.save(new Indexer());
+//	}
+//}
